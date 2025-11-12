62d5d43b92e31a4971553a726556ec0c95382a9c
9028216 <9028216@student.zadkine.nl>
2025-11-12 10:30:27 +0100

    Add files via upload

---
+++ 6.1/script.js
@@ -0,0 +1,5 @@
+function kleur_aanpassen() {
+    var headingtoadjust = document.getElementById("hello");
+    headingtoadjust.style.color = "blue";
+    headingtoadjust.innerHTML = "De kleur is aangepast naar blauw!";
+}
